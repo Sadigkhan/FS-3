ec59de49d7bfb6377729cb918ee012390e74ab99
Sadigkhan Gaykhanov <sadigkhaneg@code.edu.az>
2023-09-12 13:57:18 +0400

    Js intro

---
+++ 11.Js Intro/src/js/app.js
@@ -0,0 +1,114 @@
+// console.log("Hello world")
+// console.warn("Warning")
+// console.error("Error")
+
+
+// // Data types
+
+// Primitive data types
+
+// String
+
+// var Tamerlan="JS Developer";
+// console.log(typeof Tamerlan);
+
+//Number
+
+// var age=25;
+// console.log(typeof age);
+
+//Boolean
+
+// var isMarried=true;
+// console.log(typeof isMarried);
+
+//Undefined
+// var a;
+// console.log(typeof a);
+
+//Null
+// var a=null;
+// console.log(typeof a);
+
+
+//Operators
+
+// var a="20";
+// var b="9";
+// console.log(a%b);
+
+// var a=10;
+// var b="20";
+// // console.log(a!==b);
+// if(a!==b){
+// console.log("hi");
+// }
+// else{
+//     console.log("bye");
+// }
+
+// function LightMode(){
+//     // document.getElementById("body").style.backgroundColor="white";
+//     // document.getElementById("body").style.color="black";
+//     // document.getElementById("body").style.backgroundColor="white";
+//     // document.getElementById("body").style.color="black";
+
+//     document.getElementById("lamp").style.filter="grayScale(0)";
+// }
+
+
+// function DarkMode(){
+//     // document.getElementById("body").style.backgroundColor="black";
+//     // document.getElementById("body").style.color="white";
+//     document.getElementById("lamp").style.filter="grayScale(1)";
+
+// }
+
+// document.getElementById("username").innerHTML="<i>Rovsen</i>";
+// document.getElementById("user-name").innerText="<i>Rovsen</i>";
+// document.write("<h1>Hello <i>World</i></h1>");
+
+// var person={
+//     ad:"Rovsen",
+//     soyad:"Meherremov",
+//     yas:23,
+//     isMarried:false,
+//     birthDate:function(){
+//         return new Date().getFullYear()-this.yas;
+//     },
+//     adSoyad:function(){
+//         return this.ad+" "+this.soyad;
+//     }
+
+
+// }
+
+// function Toplama(a,b){
+// return a+b;
+// }
+
+
+// console.log(Toplama(5,6));
+// console.log(person.adSoyad());
+
+// var pi = Math.PI;
+     
+// console.log(Math.floor(4.9));
+
+
+// function Random(){
+//     let element=document.getElementById("RandText");
+//     element.innerHTML += Math.floor(Math.random()*100) + "\n";
+ 
+// }
+
+
+// confirm("Hello World");
+
+
+
+
+
+
+
+

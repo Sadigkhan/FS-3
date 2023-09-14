a44fcf2cd6a3d407dfa642cd951aa4fae2fa7290
Sadigkhan Gaykhanov <sadigkhaneg@code.edu.az>
2023-09-14 12:50:05 +0400

    String,Number

---
+++ 12.String,Number/src/js/app.js
@@ -0,0 +1,104 @@
+// var a="Hello";
+// var b='Hello';
+// var c=`Salam
+// Dunya
+// `
+// var e=new String("Hello World");
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(typeof c)
+// console.log(typeof e)
+// console.log(c)
+
+// let person={
+//     ad:"Rovsen",
+//     soyad:"Meherremov",
+//     yas:23,
+//     getFullInfo:function(){
+//         return this.ad+" "+this.soyad+" "+this.yas+" yasinda";
+//     },
+//     setFullInfo:function(){
+//         return `${this.ad} ${this.soyad} ${this.yas} yasinda`
+//     }
+// }
+
+// console.log(person.setFullInfo())
+
+
+// var ad="Gulsares";
+// var soyad="Meherremov";
+// console.log(ad.length)
+// console.log(ad.toUpperCase())
+// console.log(ad.toLowerCase())
+// console.log(ad.charAt(6))
+// console.log(ad.indexOf("s"))
+// console.log(ad.lastIndexOf("s"))
+// console.log(ad.includes("s"))
+// console.log(ad.slice(0,5))
+// console.log(ad.slice(5))
+// console.log(ad.slice(-5,-1))
+// console.log(ad.substring(0,5))
+// console.log(ad.substr(2,3))
+// console.log(ad.replace(/gulsares/ig,"Rovsen")) //i insensitive g global
+// console.log(ad.replaceAll("Gulsares","Rovsen"))
+// console.log(ad.split(" "))
+// console.log(ad.trim())
+// console.log(ad.trimStart())
+// console.log(ad.trimEnd())
+// console.log(ad.concat("  ",soyad))
+// console.log(ad.repeat(3))
+// console.log(ad.charCodeAt(0))
+
+// var num=822.123;
+// var num2=new Number(123);
+
+// console.log(num.toString())
+// console.log(num.valueOf())
+// console.log(num.toExponential())
+// console.log(num.toPrecision())
+// console.log(num.toPrecision(2))
+// console.log(num.toPrecision(4))
+
+// Number(true);
+// Number(false);
+// Number("10");
+// Number("  10");
+// Number("10  ");
+// Number(" 10  ");
+// Number("10.33");
+// Number("10,33");
+// Number("10 33");
+// Number("John");
+
+// console.log(Number(true))
+// console.log(Number(false))
+// console.log(Number("10"))
+// console.log(Number("  10"))
+// console.log(Number("10  "))
+// console.log(Number(" 10  "))
+// console.log(Number("10.33"))
+// console.log(Number("10,33"))
+// console.log(Number("10 33"))
+// console.log(Number("John"))
+
+// parseInt("-10");
+// parseInt("-10.33");
+// parseInt("10");
+// parseInt("10.33");
+// parseInt("10 20 30");
+// parseInt("10 years");
+// parseInt("years 10");
+
+// console.log(parseFloat("-10"))
+// console.log(parseFloat("-10.33"))
+// console.log(parseFloat("10"))
+// console.log(parseFloat("10.33"))
+// console.log(parseFloat("10 20 30"))
+// console.log(parseFloat("10 years"))
+// console.log(parseFloat("1.5years 10"))
+
+
+
+
+
+

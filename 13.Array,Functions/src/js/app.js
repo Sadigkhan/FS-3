ced99aab138011b70ab238a57af94a456aef0030
Sadigkhan Gaykhanov <sadigkhaneg@code.edu.az>
2023-09-16 14:03:03 +0400

    Array,Functions

---
+++ 13.Array,Functions/src/js/app.js
@@ -0,0 +1,120 @@
+let people = ["Rovsen","Eli","Tamerlan","Mehemmed","Elmeddin"];
+let surnames=["Memmedov","Eliyev","Rzayev","Xelilov","Cavadova"]
+// let numbers=[1,2,3,4,56,6,7,8,9,10];
+// console.log(numbers.sort());
+// let ages = new Array("Rovsen","Gulsare","Tamerlan","Mehemmed","Rovshenka");
+// let surnames=[];
+// surnames[0]="Rovsenov";
+// surnames[1]="Gulsareva";
+// surnames[2]="Tamerlanov";
+// surnames[4]="Mehemmedov";
+// var a;
+// console.log(typeof a);
+// console.log(typeof surnames[3]);
+// console.log(people);
+// console.log(people.toString());
+// console.log(people.join(" - "));
+// console.log(people.pop());
+// console.log(people);
+// console.log(people.push("Abdulsalam"));
+// console.log(people);
+// people[people.length]="Gulsare";
+// console.log(people.shift());
+// console.log(people);
+// console.log(people.unshift("Abdulsalam"));
+// console.log(people);
+
+// console.log(people.concat(surnames));
+// console.log(people.splice(2,0,"Abdulsalam"));
+// console.log(people);
+// delete people[2];
+// console.log(people);
+
+
+// const myArr = [[1,2,[11,12,[123,1234,[124124,123123,[12313,12312412]]]]],[3,4,[13,14]],[5,6,[15,16]],[7,8,[17,18]],[9,10,[19,20]]];
+
+// const newArr = myArr.flat(Infinity);
+
+// console.log(newArr);
+
+
+
+//function decleration
+// console.log(Topla(5,10));
+
+// function Topla(a,b){
+
+//     return a+b;
+// }
+// console.log(Topla(5,10));
+
+
+
+
+
+//function expression
+// console.log(cem(5,10));
+
+// var cem = function(a,b){
+    
+//         return a+b;
+// } 
+
+//Arrow function
+// let cem1 = (a,b) => a+b;
+
+// let cem = (a,b) => a+b;
+
+
+// let btn1=document.getElementById("first");
+// let img=document.getElementById("img");
+// let anotherbutton= document.getElementsByTagName("button");
+// console.log(anotherbutton);
+// let btn2=document.getElementById("second");
+
+
+
+// function First(){
+//     console.log(this)
+// }
+
+
+// let Second = () => console.log(this);
+
+// btn1.addEventListener("click",First);
+// btn2.addEventListener("click",Second);
+
+// btn1.addEventListener("click",ShowImage)
+
+
+// function ShowImage(){
+//     img.style.display="block";
+// }
+
+// (function(){
+//     alert("Hello World");
+// }())
+
+
+// function String(text){
+//     this.text=text;
+// }
+// let text2=new String("Hello World");
+
+// console.log(text2);
+
+
+// function Person(name,surname,age){
+//     this.name=name;
+//     this.surname=surname;
+//     this.age=age;
+//     this.fullName=function(){
+//         return this.name+" "+this.surname;
+//     }
+// }
+
+// let person1=new Person("Rovsen","Memmedov",20);
+
+// console.log(person1.surname);
+
+
